480ffe012e125ca058dc6688a2b72865c91bf514
DeepakSharma90277 <dsharma90277@gmail.com>
2026-02-03 18:03:29 +0530

    Enhanced navbar UI with gradient

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white p-4 text-center text-xl font-bold">
-      Smart Parking Lot System
+    <nav className="bg-gradient-to-r from-blue-600 to-indigo-600 text-white p-4 text-center text-xl font-bold shadow-lg">
+    🚗 Smart Parking Lot System
     </nav>
+
   );
 }
