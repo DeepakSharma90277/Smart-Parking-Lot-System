76b2947dbc10af8596ac62d4469a028c02aa74b8
DeepakSharma90277 <dsharma90277@gmail.com>
2026-02-03 18:26:34 +0530

    Updated API URL for deployed backend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/slots";
+const API = "https://smart-parking-lot-system-suby.onrender.com";
 
 export const addSlot = (data) => axios.post(`${API}/add`, data);
 export const getSlots = () => axios.get(API);
